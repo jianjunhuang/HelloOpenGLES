ebcfc72f371045722f7c5ecb43283e1bc5e5fa5d
JianjunHuang <demon903732218@gmail.com>
2020-12-16 16:30:14 +0800

    添加渐变效果

--- app/src/main/java/com/jianjun/helloopengles/airhockey/AirHockeyActivity.kt
+++ app/src/main/java/com/jianjun/helloopengles/airhockey/AirHockeyActivity.kt
@@ -29,33 +29,34 @@ class AirHockeyActivity : AppCompatActivity() {
     class AirHockeyRender(private val context: Context) : GLSurfaceView.Renderer {
 
         private var uColorLocation: Int = 0
+        private var aColorLocation: Int = 0
         private var aPositionLocation: Int = 0
 
         val tableVertices = floatArrayOf(
-            //triangle1
-            -0.5f, -0.5f, 0f,
-            0.5f, 0.5f, 0f,
-            -0.5f, 0.5f, 0f,
-            //triangle2
-            -0.5f, -0.5f, 0f,
-            0.5f, 0.5f, 0f,
-            0.5f, -0.5f, 0f,
+            //三角扇形, 后三位 R,G,B
+            0f, 0f, 0f, 1f, 1f, 1f,
+            -0.5f, -0.5f, 0f, 0.7f, 0.7f, 0.7f,
+            0.5f, -0.5f, 0f, 0.7f, 0.7f, 0.7f,
+            0.5f, 0.5f, 0f, 0.7f, 0.7f, 0.7f,
+            -0.5f, 0.5f, 0f, 0.7f, 0.7f, 0.7f,
+            -0.5f, -0.5f, 0f, 0.7f, 0.7f, 0.7f,
+
             //line1
-            -0.5f, 0f, 0f,
-            0.5f, 0f, 0f,
+            -0.5f, 0f, 0f, 0f, 0f, 0f,
+            0.5f, 0f, 0f, 0f, 0f, 0f,
             //point1
-            0f, 0.25f, 0f,
+            0f, 0.25f, 0f, 0f, 0f, 0f,
             //point2
-            0f, -0.25f, 0f,
+            0f, -0.25f, 0f, 0f, 0f, 0f,
             //border
-            -0.55f, -0.55f, 0f,
-            0.55f, 0.55f, 0f,
-            -0.55f, 0.55f, 0f,
-            -0.55f, -0.55f, 0f,
-            0.55f, 0.55f, 0f,
-            0.55f, -0.55f, 0f,
+            -0.55f, -0.55f, 0f, 0.52f, 0.73f, 0.94f,
+            0.55f, 0.55f, 0f, 0.52f, 0.73f, 0.94f,
+            -0.55f, 0.55f, 0f, 0.52f, 0.73f, 0.94f,
+            -0.55f, -0.55f, 0f, 0.52f, 0.73f, 0.94f,
+            0.55f, 0.55f, 0f, 0.52f, 0.73f, 0.94f,
+            0.55f, -0.55f, 0f, 0.52f, 0.73f, 0.94f,
             //ice ball
-            0f, 0f, 0f,
+            0f, 0f, 0f, 0.52f, 0.73f, 0.94f
         )
         val vertexData = ByteBuffer.allocateDirect(tableVertices.size * 4)//float 数字有四个字节
             .order(ByteOrder.nativeOrder())
@@ -69,6 +70,8 @@ class AirHockeyActivity : AppCompatActivity() {
 
             //顶点的分量 x,y,z
             const val POSITION_COMPONENT_COUNT = 3
+            const val COLOR_COMPONENT_COUNT = 3
+            const val STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * 4
         }
 
         override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
@@ -94,7 +97,8 @@ class AirHockeyActivity : AppCompatActivity() {
             if (program != 0) {
                 GLES20.glUseProgram(program)
             }
-            uColorLocation = GLES20.glGetUniformLocation(program, "u_Color")
+            aColorLocation = GLES20.glGetAttribLocation(program, "a_Color")
+//            uColorLocation = GLES20.glGetUniformLocation(program, "u_Color")
             aPositionLocation = GLES20.glGetAttribLocation(program, "a_Position")
 
             //make sure read data from begin
@@ -104,10 +108,22 @@ class AirHockeyActivity : AppCompatActivity() {
                 POSITION_COMPONENT_COUNT,//分量size
                 GLES20.GL_FLOAT,
                 false,//数据整型时有用
-                0,//多属性时有用
+                STRIDE,//多属性时有用
                 vertexData
             )
             GLES20.glEnableVertexAttribArray(aPositionLocation)
+
+            //link a_Color
+            vertexData.position(POSITION_COMPONENT_COUNT)//跳过位置分量
+            GLES20.glVertexAttribPointer(
+                aColorLocation,
+                COLOR_COMPONENT_COUNT,//分量size
+                GLES20.GL_FLOAT,
+                false,//数据整型时有用
+                STRIDE,//读取下一个颜色需要跳过多少个字节
+                vertexData
+            )
+            GLES20.glEnableVertexAttribArray(aColorLocation)
         }
 
         override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
@@ -118,25 +134,25 @@ class AirHockeyActivity : AppCompatActivity() {
             GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
 
             //border
-            GLES20.glUniform4f(uColorLocation, 0.52f, 0.73f, 0.94f, 1f)
+//            GLES20.glUniform4f(uColorLocation, 0.52f, 0.73f, 0.94f, 1f)
             GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 10, 6)
 
             //与 shader 中的 vec4 匹配
             // ↓ 指定颜色
-            GLES20.glUniform4f(uColorLocation, 1f, 1f, 1f, 1f)
+//            GLES20.glUniform4f(uColorLocation, 1f, 1f, 1f, 1f)
             // ↓ 绘制 2 个三角形，从开头读取 6 个顶点
-            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6)
+            GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 6)
 
             //draw line
-            GLES20.glUniform4f(uColorLocation, 0f, 0f, 0f, 1f)
+//            GLES20.glUniform4f(uColorLocation, 0f, 0f, 0f, 1f)
             GLES20.glDrawArrays(GLES20.GL_LINES, 6, 2)
 
             //draw point
-            GLES20.glUniform4f(uColorLocation, 0f, 0f, 0f, 1f)
+//            GLES20.glUniform4f(uColorLocation, 0f, 0f, 0f, 1f)
             GLES20.glDrawArrays(GLES20.GL_POINTS, 8, 2)
 
             //draw ice ball
-            GLES20.glUniform4f(uColorLocation, 0.52f, 0.73f, 0.94f, 1f)
+//            GLES20.glUniform4f(uColorLocation, 0.52f, 0.73f, 0.94f, 1f)
             GLES20.glDrawArrays(GLES20.GL_POINTS, 16, 1)
         }
     }
--- app/src/main/res/raw/simple_vertex_shader.glsl
+++ app/src/main/res/raw/simple_vertex_shader.glsl
@@ -4,8 +4,14 @@
  attribute -> 顶点属性
 */
 attribute vec4 a_Position;
+attribute vec4 a_Color;//r,g,b,a default 1
+
+varying vec4 v_Color;
 
 void main() {
+
+    v_Color = a_Color;
+
     //must init the gl_Position
     gl_Position = a_Position;
     gl_PointSize = 10.0;//gl_Position为中心的四边形，四条边的长度等于 gl_PointSize
