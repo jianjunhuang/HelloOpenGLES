7a55642e4a44dabc1b19d4953feb1a5bae841452
JianjunHuang <demon903732218@gmail.com>
2020-12-16 18:05:42 +0800

    fix table size

--- app/src/main/java/com/jianjun/helloopengles/airhockey/AirHockeyActivity.kt
+++ app/src/main/java/com/jianjun/helloopengles/airhockey/AirHockeyActivity.kt
@@ -3,6 +3,7 @@ package com.jianjun.helloopengles.airhockey
 import android.content.Context
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
+import android.opengl.Matrix
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -31,30 +32,45 @@ class AirHockeyActivity : AppCompatActivity() {
         private var uColorLocation: Int = 0
         private var aColorLocation: Int = 0
         private var aPositionLocation: Int = 0
+        private val projectMatrix = FloatArray(16)
+        private var uMatrixLocation = 0
 
+        // x, y, z, R, G, B
         val tableVertices = floatArrayOf(
             //三角扇形, 后三位 R,G,B
             0f, 0f, 0f, 1f, 1f, 1f,
-            -0.5f, -0.5f, 0f, 0.7f, 0.7f, 0.7f,
-            0.5f, -0.5f, 0f, 0.7f, 0.7f, 0.7f,
-            0.5f, 0.5f, 0f, 0.7f, 0.7f, 0.7f,
-            -0.5f, 0.5f, 0f, 0.7f, 0.7f, 0.7f,
-            -0.5f, -0.5f, 0f, 0.7f, 0.7f, 0.7f,
+            -0.5f, -0.8f, 0f, 0.7f, 0.7f, 0.7f,
+            -0.25f, -0.8f, 0f, 0.7f, 0.7f, 0.7f,
+            0f, -0.8f, 0f, 0.7f, 0.7f, 0.7f,
+            0.25f, -0.8f, 0f, 0.7f, 0.7f, 0.7f,
+            0.5f, -0.8f, 0f, 0.7f, 0.7f, 0.7f,
+            0.5f, -0.55f, 0f, 0.7f, 0.7f, 0.7f,
+            0.5f, 0f, 0f, 0.7f, 0.7f, 0.7f,
+            0.5f, 0.55f, 0f, 0.7f, 0.7f, 0.7f,
+            0.5f, 0.8f, 0f, 0.7f, 0.7f, 0.7f,
+            0.25f, 0.8f, 0f, 0.7f, 0.7f, 0.7f,
+            0f, 0.8f, 0f, 0.7f, 0.7f, 0.7f,
+            -0.25f, 0.8f, 0f, 0.7f, 0.7f, 0.7f,
+            -0.5f, 0.8f, 0f, 0.7f, 0.7f, 0.7f,
+            -0.5f, 0.35f, 0f, 0.7f, 0.7f, 0.7f,
+            -0.5f, 0f, 0f, 0.7f, 0.7f, 0.7f,
+            -0.5f, -0.35f, 0f, 0.7f, 0.7f, 0.7f,
+            -0.5f, -0.8f, 0f, 0.7f, 0.7f, 0.7f,
 
             //line1
             -0.5f, 0f, 0f, 0f, 0f, 0f,
-            0.5f, 0f, 0f, 0f, 0f, 0f,
+            0.5f, 0f, 0f, 1f, 1f, 1f,
             //point1
             0f, 0.25f, 0f, 0f, 0f, 0f,
             //point2
             0f, -0.25f, 0f, 0f, 0f, 0f,
             //border
-            -0.55f, -0.55f, 0f, 0.52f, 0.73f, 0.94f,
-            0.55f, 0.55f, 0f, 0.52f, 0.73f, 0.94f,
-            -0.55f, 0.55f, 0f, 0.52f, 0.73f, 0.94f,
-            -0.55f, -0.55f, 0f, 0.52f, 0.73f, 0.94f,
-            0.55f, 0.55f, 0f, 0.52f, 0.73f, 0.94f,
-            0.55f, -0.55f, 0f, 0.52f, 0.73f, 0.94f,
+            -0.55f, -0.85f, 0f, 0.52f, 0.73f, 0.94f,
+            0.55f, 0.85f, 0f, 0.52f, 0.73f, 0.94f,
+            -0.55f, 0.85f, 0f, 0.52f, 0.73f, 0.94f,
+            -0.55f, -0.85f, 0f, 0.52f, 0.73f, 0.94f,
+            0.55f, 0.85f, 0f, 0.52f, 0.73f, 0.94f,
+            0.55f, -0.85f, 0f, 0.52f, 0.73f, 0.94f,
             //ice ball
             0f, 0f, 0f, 0.52f, 0.73f, 0.94f
         )
@@ -72,6 +88,21 @@ class AirHockeyActivity : AppCompatActivity() {
             const val POSITION_COMPONENT_COUNT = 3
             const val COLOR_COMPONENT_COUNT = 3
             const val STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * 4
+
+            const val FIRST_TABLE = 0
+            const val SIZE_TABLE = 18
+
+            const val FIRST_LINE = 18
+            const val SIZE_LINE = 2
+
+            const val FIRST_POINT = 20
+            const val SIZE_POINT = 2
+
+            const val FIRST_BORDER = 22
+            const val SIZE_BORDER = 6
+
+            const val FIRST_BALL = 28
+            const val SIZE_BALL = 1
         }
 
         override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
@@ -124,36 +155,49 @@ class AirHockeyActivity : AppCompatActivity() {
                 vertexData
             )
             GLES20.glEnableVertexAttribArray(aColorLocation)
+
+            uMatrixLocation = GLES20.glGetUniformLocation(program, "u_Matrix")
         }
 
         override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
             GLES20.glViewport(0, 0, width, height)
+            val aspectRatio = if (width > height) {
+                width / height.toFloat()
+            } else {
+                height / width.toFloat()
+            }
+            if (width > height) {
+                //landscape
+                Matrix.orthoM(projectMatrix, 0, -aspectRatio, aspectRatio, -1f, 1f, -1f, 1f)
+            } else {
+                Matrix.orthoM(projectMatrix, 0, -1f, 1f, -aspectRatio, aspectRatio, -1f, 1f)
+            }
         }
 
         override fun onDrawFrame(gl: GL10?) {
             GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
-
+            GLES20.glUniformMatrix4fv(uMatrixLocation, 1, false, projectMatrix, 0)
             //border
 //            GLES20.glUniform4f(uColorLocation, 0.52f, 0.73f, 0.94f, 1f)
-            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 10, 6)
+            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, FIRST_BORDER, SIZE_BORDER)
 
             //与 shader 中的 vec4 匹配
             // ↓ 指定颜色
 //            GLES20.glUniform4f(uColorLocation, 1f, 1f, 1f, 1f)
             // ↓ 绘制 2 个三角形，从开头读取 6 个顶点
-            GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 6)
+            GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, FIRST_TABLE, SIZE_TABLE)
 
             //draw line
 //            GLES20.glUniform4f(uColorLocation, 0f, 0f, 0f, 1f)
-            GLES20.glDrawArrays(GLES20.GL_LINES, 6, 2)
+            GLES20.glDrawArrays(GLES20.GL_LINES, FIRST_LINE, SIZE_LINE)
 
             //draw point
 //            GLES20.glUniform4f(uColorLocation, 0f, 0f, 0f, 1f)
-            GLES20.glDrawArrays(GLES20.GL_POINTS, 8, 2)
+            GLES20.glDrawArrays(GLES20.GL_POINTS, FIRST_POINT, SIZE_POINT)
 
             //draw ice ball
 //            GLES20.glUniform4f(uColorLocation, 0.52f, 0.73f, 0.94f, 1f)
-            GLES20.glDrawArrays(GLES20.GL_POINTS, 16, 1)
+            GLES20.glDrawArrays(GLES20.GL_POINTS, FIRST_BALL, SIZE_BALL)
         }
     }
 
